a8b707344a8db81aaf109a6f5e938064e541c94e
sellstep <stephen_p_sell@mcpsmd.org>
2020-10-10 12:00:59 -0400

    Create DistanceFormula.java

---
+++ DistanceFormula.java
@@ -0,0 +1,28 @@
+/*
+Name
+Date
+FileName.java
+brief description of what file does
+*/
+
+public class DistanceFormula
+{
+   public static void main(String[] args)
+   {
+       int xOne, yOne, xTwo, yTwo;
+       //initialize value of xOne, yOne, xTwo, yTwo
+
+
+       //calculate squared difference of the x-values
+
+
+       //Calculate squared difference of the y-values
+
+
+       //Add the squared differences and take the square root
+
+
+       //Print statement to output the distance
+
+   }
+}
